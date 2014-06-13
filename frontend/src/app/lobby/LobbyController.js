7955088666d0a0ad7bb1f18e0bb3975f90bcb4fc
tarlepp <tarmo.leppanen@kase.fi>
2014-06-14 02:16:20 +0300

    Small fine tune for layout and added focus to input on 'join game' screen.

--- frontend/src/app/lobby/LobbyController.js
+++ frontend/src/app/lobby/LobbyController.js
@@ -1,17 +1,23 @@
 (function() {
     angular.module('frontend.controllers')
         .controller('LobbyController',
-        [
-            '$scope', '$state', 'Auth',
-            function($scope, $state, Auth) {
-                if (Auth.isAuthenticated()) {
-                    $state.go("game.game")
+            [
+                '$scope', '$state', 'Auth',
+                function($scope, $state, Auth) {
+                    if (Auth.isAuthenticated()) {
+                        $state.go('game.game');
+                    }
+
+                    angular.element('#nick').focus();
+
+                    $scope.join = function() {
+                        Auth
+                            .join($scope.nick)
+                            .then(function() {
+                                $state.go('game.game');
+                            });
+                    };
                 }
-                $scope.join = function() {
-                    Auth
-                        .join($scope.nick)
-                        .then(function() { $state.go("game.game"); });
-                };
-            }
-        ])
+            ]
+        );
 }());
