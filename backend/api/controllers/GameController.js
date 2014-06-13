bc70057a8880176a368296d2a093f254f47f614b
ara <anttirapa@gmail.com>
2014-06-13 19:10:32 +0300

    Added Game model and controller

---
+++ backend/api/controllers/GameController.js
@@ -0,0 +1,11 @@
+/**
+ * GameController
+ *
+ * @description :: Server-side logic for managing Games
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
